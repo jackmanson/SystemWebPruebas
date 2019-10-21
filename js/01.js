d995ec98e30ceb14808f2e0ccc6923e35e142cd9
jackmanson <jassonpolo@gmail.com>
2019-10-21 15:22:56 -0500

    Creacion 01.html y carpeta JS

---
+++ js/01.js
@@ -0,0 +1,15 @@
+"use strict"
+
+var numero = 10;
+console.log(numero);
+
+let local = 50;
+console.log(local);
+
+numero = "diez";
+console.log(numero);
+
+const pi = 3.14;
+
+var res = pi*5;
+console.log(res);
