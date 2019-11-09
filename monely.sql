1b072363b8193a4f936ac243be58c0d6b13f9ba4
jackmanson <jassonpolo@gmail.com>
2019-11-09 16:51:04 -0500

    actualizacion base datos monely

--- monely.sql
+++ monely.sql
@@ -716,6 +716,20 @@ CREATE TABLE `envios_planchado`(
 );
 
 
+-- TABLA PAISES Y NACIONALIDADES
+CREATE TABLE `paises_nacionalidad`(
+	`id_pais` INT
+	`pais_nacionalidad` VARCHAR
+);
+
+
+-- TABLA INTERMEDIA ESTILOS Y TALLAS
+CREATE TABLE `medidaPrendasConfeccion_tallas`(
+	fk_id_prenda_confeccion
+	fk_id_talla 
+	`curva_talla` INT
+);
+
 
 
 
@@ -734,6 +748,44 @@ CREATE TABLE `patronaje_moldes`( -- muestra tambien Ubicacion de diseño total, 
 	fk_id_tipo_prenda_3
 	fk_id_estado_actividad_19  -- estado usuario --> activo - verde / usuario transitivo o temporal - naranja / inactivo - rojo
 );
+
+
+-- REVISAR ESTAS TABLAS DERIVADAS DE LA TABLA PATRONAJE
+-- TABLA MEDIDAS DE PRENDA PARA CONFECCION
+CREATE TABLE `medidas_prenda_confeccion`(
+	`id_prenda_confeccion` INT
+	`numero_molde` VARCHAR
+	`fecha_ultima_actualizacion` DATETIME
+	`fecha_registro` DATETIME
+	fk_id_orden_guia_corte
+	fk_id_cliente
+	fk_id_estilo
+	fk_id_pais
+	fk_id_marca
+	fk_id_color_rip
+	fk_id_color_tela
+	fk_id_tipo_tela
+	fk_id_ubicacion
+);
+
+
+-- TABLA DE UBICACION DE DISEÑO A ESTAMPAR
+CREATE TABLE `ubicacion_diseño`(
+	`id_ubicacion` INT
+	`imagen_vista_delantero` VARCHAR
+	`descripcion_vista_delantero` VARCHAR
+	`imagen_vista_espalda` VARCHAR
+	`descripcion_vista_espalda` VARCHAR
+	
+	`referencia` VARCHAR -- Referencia que se toma para la ubicacion
+	`medida_1` DECIMAL -- refencia un ancho
+	`medida_2` DECIMAL -- referencia un largo
+);
+
+
+
+
+
 
 
 -- TABLA CODIGO PRENDA DE PRODUCCION  -- revisar
