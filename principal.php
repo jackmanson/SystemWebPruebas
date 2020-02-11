a42bdf22e937cecca2d6ef02cb74b52ed3c3b47c
jack <jackmanson1001@gmail.com>
2020-02-11 14:18:59 -0500

    Agregando plantilla con estructura MVC

---
+++ principal.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>process</title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="css/main.css"/>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+	<h1>Procesos</h1>
+	<?= "hola mundo";?> 
+
+	
+</body>
+</html>
